1e37f52e2e7826387767169b7639ad86886ff1c9
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-06-03 16:25:40 +0200

    >

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,12 @@
 import bodyParser from "body-parser";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import pg from "pg";
-
-// Connect to the database using the DATABASE_URL environment
-//   variable injected by Railway
-const pool = new pg.Pool();
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
-
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.json({ message: "hello from node" });
 });
 
 app.listen(port, () => {
